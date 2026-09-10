7a2f89ffd91a7d8723b553bb09df86313aa1a705
Rodriguez-343 <edwinrodriguez10moa@gmail.com>
2026-09-09 18:39:50 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Pega aquí los dos valores de tu proyecto de Supabase:
+// Panel de Supabase -> Project Settings -> API
+window.SUPABASE_URL = "https://ljftbcpjghuxcuubtjwh.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqZnRiY3BqZ2h1eGN1dWJ0andoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5OTc0OTQsImV4cCI6MjEwNDU3MzQ5NH0.oasU5L4cv--FitX5uFbPNVf6U6K0vLksPk4EysZy9t8";
